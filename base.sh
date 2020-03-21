e6708f501207b835000f55638fbf77fc8009f683
Kay0u <pierre.moltess@gmail.com>
2020-03-21 18:10:20 +0100

    Fix DEFAULT_BRANCH

--- base.sh
+++ base.sh
@@ -15,14 +15,14 @@
 #CUSTOM_ENV_CI_PROJECT_DIR=/builds/yunohost/yunohost
 CONTAINER_ID="runner-$CUSTOM_ENV_CI_RUNNER_ID-project-$CUSTOM_ENV_CI_PROJECT_ID-concurrent-$CUSTOM_ENV_CI_CONCURRENT_PROJECT_ID-$CUSTOM_ENV_CI_JOB_ID"
 ARCH="$(echo $CUSTOM_ENV_CI_RUNNER_EXECUTABLE_ARCH | cut -d'/' -f2)" # linux/amd64
-DEFAULT_BRANCH=$(echo $CUSTOM_ENV_CI_DEFAULT_BRANCH | cut -d'-' -f1) # stretch-unstable, buster-unstable...
+DEFAULT_BRANCH="$CUSTOM_ENV_CI_DEFAULT_BRANCH"
 CURRENT_VERSION=$(echo $CUSTOM_ENV_CI_DEFAULT_BRANCH | cut -d'-' -f2) # stretch-unstable, stretch-testing, stretch-stable...
 CURRENT_BRANCH="$CUSTOM_ENV_CI_COMMIT_REF_NAME"
 DEBIAN_VERSION=$(echo $CUSTOM_ENV_CI_COMMIT_REF_NAME | cut -d'-' -f1) # CUSTOM_ENV_CI_COMMIT_REF_NAME is the target branch of the MR: stretch-unstable, buster-unstable...
 if [ -z "$DEBIAN_VERSION" ] || [ "$DEBIAN_VERSION" != "stretch" ] && [ "$DEBIAN_VERSION" != "buster" ]
 then
-    echo "Use the default branch $DEFAULT_BRANCH"
-    DEBIAN_VERSION="$DEFAULT_BRANCH"
+    DEBIAN_VERSION="$(echo $CUSTOM_ENV_CI_DEFAULT_BRANCH | cut -d'-' -f1)" # stretch-unstable, buster-unstable...
+    echo "Use the default debian version: $DEBIAN_VERSION"
 fi
 SNAPSHOT_NAME="$CUSTOM_ENV_CI_JOB_IMAGE"
 if [ -z "$SNAPSHOT_NAME" ]
