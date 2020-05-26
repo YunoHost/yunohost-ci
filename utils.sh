f52d24618c6976b81b1e7b648043a818d77a4869
Kay0u <pierre@kayou.io>
2020-05-26 19:32:46 +0200

    create prints.sh

--- utils.sh
+++ utils.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $current_dir/prints.sh
 source $current_dir/variables.sh # Get variables from variables.
 
 clean_containers()
