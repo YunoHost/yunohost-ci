c31395dfa7869839867faf0e9086a1d5e3b27b6b
Kay0u <pierre@kayou.io>
2022-01-05 18:27:59 +0100

    drop python2, add more preinstalled pip3 packages

--- utils.sh
+++ utils.sh
@@ -147,8 +147,7 @@ get_dependencies()
 	# Same as above, except that all dependencies are in the same line
 	SSOWAT_DEPENDENCIES=$(curl https://raw.githubusercontent.com/YunoHost/ssowat/dev/debian/control 2> /dev/null | grep '^Depends:' | sed 's/Depends://' | sed -e "s/,//g" -e "s/[(][^)]*[)]//g" -e "s/ | \S\+//g" | tr "\n" " ")
 	BUILD_DEPENDENCIES="git-buildpackage postfix python-setuptools python3-pip devscripts"
-	PIP_PKG="mock pip pytest pytest-cov pytest-mock pytest-sugar requests-mock tox ansi2html"
-	PIP3_PKG="mock pip pytest pytest-cov pytest-mock pytest-sugar requests-mock tox ansi2html black jinja2"
+	PIP3_PKG="mock pip pytest pytest-cov pytest-mock pytest-sugar requests-mock tox ansi2html black jinja2 types-ipaddress, types-enum34, types-cryptography, types-toml, types-requests, types-PyYAML, types-pyOpenSSL, types-mock"
 }
 
 rebuild_base_containers()
@@ -197,7 +196,6 @@ rebuild_base_containers()
 
 	# Pre install dependencies
 	lxc exec "$base_image_to_rebuild-tmp" -- /bin/bash -c "DEBIAN_FRONTEND=noninteractive SUDO_FORCE_REMOVE=yes apt-get --assume-yes -o Dpkg::Options::=\"--force-confold\" install --assume-yes $YUNOHOST_DEPENDENCIES $YUNOHOST_RECOMMENDS $MOULINETTE_DEPENDENCIES $SSOWAT_DEPENDENCIES $BUILD_DEPENDENCIES"
-	lxc exec "$base_image_to_rebuild-tmp" -- /bin/bash -c "python -m pip install -U $PIP_PKG"
 	lxc exec "$base_image_to_rebuild-tmp" -- /bin/bash -c "python3 -m pip install -U $PIP3_PKG"
 
 	# Disable apt-daily
@@ -248,7 +246,6 @@ update_image() {
 	get_dependencies
 
 	lxc exec "$image_to_update-tmp" -- /bin/bash -c "DEBIAN_FRONTEND=noninteractive SUDO_FORCE_REMOVE=yes apt-get --assume-yes -o Dpkg::Options::=\"--force-confold\" install --assume-yes $YUNOHOST_DEPENDENCIES $YUNOHOST_RECOMMENDS $MOULINETTE_DEPENDENCIES $SSOWAT_DEPENDENCIES $BUILD_DEPENDENCIES"
-	lxc exec "$image_to_update-tmp" -- /bin/bash -c "python -m pip install -U $PIP_PKG"
 	lxc exec "$image_to_update-tmp" -- /bin/bash -c "python3 -m pip install -U $PIP3_PKG"
 
 	rotate_image "$image_to_update-tmp" "$image_to_update"
