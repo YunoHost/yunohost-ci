b5e48b2e3402dddaf8700baa6da4c4a587bfa08b
Kay0u <pierre@kayou.io>
2023-05-17 20:18:34 +0200

    fix prepare and cleanup scripts

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,6 @@ current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $current_dir/prints.sh
 source $current_dir/variables.sh # Get variables from variables.
 
-info "Stopping container $CONTAINER_ID"
+info "Stopping container $CONTAINER_IMAGE"
 
-lxc stop "$CONTAINER_ID"
+lxc stop "$CONTAINER_IMAGE"
